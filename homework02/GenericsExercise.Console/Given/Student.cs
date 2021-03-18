7c1f373cec65c04356fdecfa6aa45d2ea197a864
Mihai Draghia <draghia_mihai99@yahoo.com>
2021-03-18 19:20:21 +0200

    h2

---
+++ homework02/GenericsExercise.Console/Given/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericsExercise.Console
+{
+    public class Student : IEntity
+    {
+        public string Id { get; set; }
+
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        
+        public Student(string id, string first, string last)
+		{
+            Id = id;
+            FisrtName = first;
+            LastName = last;
+		}
+
+        public Student()
+		{
+
+		}
+
+        public override string ToString()
+        {
+
+            return new string(Id + " " + FisrtName + " " + LastName);
+        }
+
+    }
+}
---
+++ homework02/GenericsExercise.Console/Given/University.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericsExercise.Console
+{
+    public class University : IEntity
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public University(string id, string name, string address)
+		{
+            Id = id;
+            Name = name;
+            Address = address;
+		}
+
+        public University()
+		{
+
+		}
+		public override string ToString()
+		{
+			
+            return  new string(Id + " " + Name + " " + Address);
+		}
+	}
+}

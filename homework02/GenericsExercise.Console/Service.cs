213c71cab7b9e4327029df142837626cc1b95ca2
Mihai Draghia <draghia_mihai99@yahoo.com>
2021-03-18 19:40:25 +0200

    x

---
+++ homework02/GenericsExercise.Console/Service.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsExercise.Console
+{
+	public class Service<T> where T : IEntity
+	{
+		public Persistence Db { get; set; }
+
+		public Service()
+		{
+			Db = new Persistence();
+		}
+
+		public void Insert(T el)
+		{
+			try
+			{
+
+				Db.InsertAsync<T>(el).Wait();
+
+			}
+			catch(Exception e)
+			{
+				System.Console.WriteLine(e);
+			}
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			IEnumerable<T> res = new List<T>();
+
+			try
+			{
+				res = Db.GetAllAsync<T>().Result;
+
+				if(res == null)
+				{
+					throw new Exception();
+				}
+				
+			}
+			catch(Exception e)
+			{
+				System.Console.WriteLine(e);
+			}
+
+
+			return res;
+		}
+	}
+}
